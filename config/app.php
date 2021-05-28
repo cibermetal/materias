c04f4c5be843dfb98434f0af7586f83d1f67db46
cibermetal <david.cibermetal@gmail.com>
2021-05-28 19:36:31 -0400

    proced7

--- config/app.php
+++ config/app.php
@@ -52,7 +52,7 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'http://prueba-conocimiento.herokuapp.com/'),
+    'url' => env('APP_URL', 'https://prueba-conocimiento.herokuapp.com/'),
 
     'asset_url' => env('ASSET_URL', null),
 
